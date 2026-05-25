797ecc708c9f79013f6b42ef12e8cef0408a0628
Laxmi Priya <pendyalapriya11@gmail.com>
2026-05-25 22:12:54 +0530

    Adding SpringBoot project created from Spring Initializr

---
+++ src/test/java/com/laxmipriya/WebProject/WebProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laxmipriya.WebProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
