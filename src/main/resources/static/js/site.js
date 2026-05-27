a81e194c07804e5dc39b00a73580b6b85f9886e5
Laxmi Priya <pendyalapriya11@gmail.com>
2026-05-27 21:07:05 +0530

    added toggle card

--- src/main/resources/static/js/site.js
+++ src/main/resources/static/js/site.js
@@ -1,1 +1,22 @@
 console.log("Hello from JS");
+const users = [
+    {
+        name: "John",
+        gender: "Male",
+        img: "img/image-john.png"
+    },
+    {
+        name: "Jane",
+        gender: "Female",
+        img: "img/image-jane.png"
+    }
+];
+var currentUserIndex = 0;
+
+function toggleCard() {
+    currentUserIndex = (currentUserIndex+1)%2;
+    var user = users[currentUserIndex];
+    document.getElementById("userImage").src = user.img;
+    document.getElementById("userGender").innerHTML = user.gender;
+    document.getElementById("userName").innerHTML = user.name;
+}
